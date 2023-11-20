f5df990c7f4edca31ae9f3efb342f77fb4f77030
bartekelm <bartekelm@gmail.com>
2023-11-20 19:06:04 +0100

    Update streamlit_app.py
    add dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,9 +41,10 @@ st.dataframe (fruityvice_normalized)
 
 # snowflake
 my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_cur = my_cnx.cursor
+my_cur.execute("select * from fruit load list")
 my_data_row = my_cur.fetchone()
-st.text("Hello from Snowflake:")
-st.text(my_data_row)
+st.text("The fruit load list contains:")
+st.dataframe(my_data_row)
+
 
